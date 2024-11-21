4d1942dadf9f4949ac4d45c2917f9c31420ccab7
Diky Arga <dikyarga.id@gmail.com>
2024-11-21 17:10:14 +0800

    implement audio

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+
+const audio = new Audio("http://taira-komori.jpn.org/sound/game01/coin01.mp3");
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(30);
+
+  useState(() => {
+    const timer = setInterval(() => {
+      setCount((prevCount) => {
+        if (prevCount <= 1) {
+          audio.play();
+          return 30; // Reset to 30 when reaching 0
+        }
+        return prevCount - 1;
+      });
+    }, 1000);
+
+    // Cleanup interval on unmount
+    return () => clearInterval(timer);
+  });
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <h1>RBI playground - audio test</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        <button onClick={() => audio.play()}>manual play</button>
+        <p>Auto play in: {count}s</p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
